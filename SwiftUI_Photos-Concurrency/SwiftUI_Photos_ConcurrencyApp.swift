7017ceacbe93892738c80863cec35afc631f15ce
MradulKumar <mradul.iimt@gmail.com>
2024-09-19 14:20:38 +0530

    Initial Commit

---
+++ SwiftUI_Photos-Concurrency/SwiftUI_Photos_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Photos_ConcurrencyApp.swift
+//  SwiftUI_Photos-Concurrency
+//
+//  Created by Mradul Kumar on 19/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Photos_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
